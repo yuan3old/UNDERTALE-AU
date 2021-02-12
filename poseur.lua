ee0d1d8d62fb54f7aaca1dbc808d75698b3fd6eb
yuan3old <62503588+yuan3old@users.noreply.github.com>
2021-02-12 22:12:18 +0800

    2.12修改

--- poseur.lua
+++ poseur.lua
@@ -1,13 +1,13 @@
 -- A basic monster script skeleton you can copy and modify for your own creations.
-comments = {"It's boring\any good ideas?.", "Poseur is posing like his\rlife depends on it.", "Poseur's limbs shouldn't be\rmoving in this way."}
-commands = {"HELP", "FUCK", "WAIT"}
+comments = {"It's boring\rany good ideas?.", "Poseur is posing like his\rlife depends on it.", "Poseur's limbs shouldn't be\rmoving in this way."}
+commands = {"HELP", "F**K", "WAIT"}
 randomdialogue = {"Random\nDialogue\n1.", "Random\nDialogue\n2.", "Random\nDialogue\n3."}
 
 sprite = "poseur" --Always PNG. Extension is added automatically.
 name = "Poseur"
-hp = 60
-atk = 100
-def = 100
+hp = 10
+atk = 30
+def = 10
 check = "Check message goes here."
 dialogbubble = "right" -- See documentation for what bubbles you have available.
 canspare = false
@@ -24,15 +24,17 @@ end
  
 -- This handles the commands; all-caps versions of the commands list you have above.
 function HandleCustomCommand(command)
-    if command == "FUCK" then
-		BattleDialog({"F U C K  Y O U  T O O"})
-		currentdialogue = {"Fuck you\n too."}
+    if command == "F***" then
+		BattleDialog({"F * * *  Y O U  T O O"})
+		currentdialogue = {"F**k you\r too."}
+		canspare=false
 	elseif command == "HELP" then
-		BattleDialog({"What do you want to do when you\n have time."})
-        currentdialogue = {"Keep it\n down."}
+		BattleDialog({"What do you want to do when you\r have time."})
+        currentdialogue = {"Keep it\r down."}
+        canspare=true
     elseif command == "WAIT" then
-		BattleDialog({"What do you do besides being in\n a daze."})
-        currentdialogue = {"Wait you \ntoo."}
+		BattleDialog({"What do you do besides being in\r a daze."})
+        currentdialogue = {"Wait you \rtoo."}
     end
     
 end
