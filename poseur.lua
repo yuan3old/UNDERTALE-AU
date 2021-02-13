938964b0332f040e4198b7cc3022bbc85f7370a3
yuan3old <62503588+yuan3old@users.noreply.github.com>
2021-02-13 21:51:25 +0800

    Update poseur.lua

--- poseur.lua
+++ poseur.lua
@@ -1,13 +1,12 @@
--- A basic monster script skeleton you can copy and modify for your own creations.
 comments = {"It's boring\rany good ideas?.", "Poseur is posing like his\rlife depends on it.", "Poseur's limbs shouldn't be\rmoving in this way."}
 commands = {"HELP", "F**K", "WAIT"}
 randomdialogue = {"Random\nDialogue\n1.", "Random\nDialogue\n2.", "Random\nDialogue\n3."}
 
 sprite = "poseur" --Always PNG. Extension is added automatically.
 name = "Poseur"
-hp = 10
-atk = 30
-def = 10
+hp = 180
+atk = 100
+def = 0
 check = "Check message goes here."
 dialogbubble = "right" -- See documentation for what bubbles you have available.
 canspare = false
@@ -15,14 +14,13 @@ cancheck = true
 
 -- Happens after the slash animation but before 
 function HandleAttack(attackstatus)
-    if attackstatus == -1 then
+    if attackstatus == -4 then
         -- player pressed fight but didn't press Z afterwards
     else
         -- player did actually attack
     end
 end
  
--- This handles the commands; all-caps versions of the commands list you have above.
 function HandleCustomCommand(command)
     if command == "F***" then
 		BattleDialog({"F * * *  Y O U  T O O"})
